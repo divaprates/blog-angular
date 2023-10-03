4d0c1e0d5265c8846b554eb46f00b09c70e3a426
divaprates <divasilvaprates@gmail.com>
2023-10-03 18:38:05 -0300

    feat: Agregando multiplos css para um componente

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.scss']
+  styleUrls: ['./menu-title.component.scss', './menu-title.responsive.component.scss']
 })
 export class MenuTitleComponent {
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.scss']
+  styleUrls: ['./small-card.component.scss', './small-card.responsive.component.scss']
 })
 export class SmallCardComponent {
 
