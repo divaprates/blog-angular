ef7c65c0388af95927af3e92f271e00046170dc7
divaprates <divasilvaprates@gmail.com>
2023-10-04 19:28:28 -0300

    feat: Desacoplando os componentes da parte visual

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.scss', './small-card.responsive.component.scss']
 })
 export class SmallCardComponent {
+
+  @Input()
+  photoCover: string = '';
+
+  @Input()
+  cardTitle: string = '';
 
 }
