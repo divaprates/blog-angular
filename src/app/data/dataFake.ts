0aac86850c8357d0e7fb8e708f31b35164c990e3
divaprates <divasilvaprates@gmail.com>
2023-10-04 21:05:22 -0300

    feat: Criando outras páginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": 'src/app/pages/home/home.component.html',
+        "title": 'Dare to live the life',
+        "description": ''
+    },
+    {
+        "id": 2,
+        "photo": 'https://discover-your-south-america.com/blog/wp-content/uploads/2019/04/Pantanal-Wetlands.jpg',
+        "title": 'Collect moments',
+        "description": ''
+    },
+    {
+        "id": 3,
+        "photo": 'https://thumbs.dreamstime.com/b/spring-summer-landscape-blue-sky-clouds-river-boat-green-trees-narew-countryside-grass-poland-water-leaves-58070004.jpg',
+        "title": 'Some beautiful place to get lost',
+        "description": ''
+    }
+]
