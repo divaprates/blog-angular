8dd46ebbe6b87c4450cca1a6ba422f76def0ed08
divaprates <divasilvaprates@gmail.com>
2023-10-04 22:08:56 -0300

    feat: Deixando a content page dinamica

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -22,12 +23,19 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.router.paramMap.subscribe(value => {
-      console.log('Valor::', value.get("id"))
+      this.id = value.get('id');
+      this.setValuesToComponent(this.id);
     })
   }
 
-  setValuesToComponent(id: string) {
-    
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id == id)[0];
+
+    if(result) {
+      this.photoCover = result.photo;
+      this.contentTitle = result.title;
+      this.contentDescription = result.description;
+    }
   }
 
 }
